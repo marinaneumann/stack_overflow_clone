f021953cc01b09325977bb178b14b82fde061cce
Marina Neumann <marinaneumann2468@yahoo.com>
2014-09-22 11:54:12 -0700

    Establish associations that aanswer belongs to a user and a question.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,6 @@
 class Answer <ActiveRecord::Base
   validates :question_id, :user_id, :presence => true
 
-
+  belongs_to :user
+  belongs_to :question
 end
--- spec/answer_spec.rb
+++ spec/answer_spec.rb
@@ -3,4 +3,7 @@ describe Answer do
   it{should validate_presence_of :question_id}
   it{should validate_presence_of :user_id}
 
+  it{should belong_to :user}
+  it{should belong_to :question}
+
 end
